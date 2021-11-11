a9e5fe4cba16cadf197911d5e2463b548451633d
AJAY SURESH SJ <93826214+Ajay-2004@users.noreply.github.com>
2021-11-11 02:43:05 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,12 @@ async def start(bot: Client, cmd: Message):
             elif Config.FORWARD_AS_COPY is False:
                 send_stored_file = await bot.forward_messages(chat_id=cmd.from_user.id, from_chat_id=Config.DB_CHANNEL,
                                                               message_ids=file_id)
-             
+  await send_stored_file.reply_text(
+                f"**Join Us:** https://t.me/Doflix_studios",
+                disable_web_page_preview=True, quote=True)
+        except Exception as err:
+            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
+           
 @Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.edited)
 async def main(bot: Client, message: Message):
     if message.chat.type == "private":
